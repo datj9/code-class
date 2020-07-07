1cae03a3b5d08fe233c12acd219eee94b43120ea
tandat198 <tandat198@gmail.com>
2020-07-07 16:19:38 +0700

    Updates

---
+++ precache-manifest.3fa581bdfb0a540d24f886b2731ec74c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517f80cd82c0fe56887d2cbd506f3098",
+    "url": "/code-class/index.html"
+  },
+  {
+    "revision": "9264bb77e7c0a42c86a6",
+    "url": "/code-class/static/css/2.58771291.chunk.css"
+  },
+  {
+    "revision": "dddb77bfc314115d4bed",
+    "url": "/code-class/static/css/main.cb0bcedc.chunk.css"
+  },
+  {
+    "revision": "9264bb77e7c0a42c86a6",
+    "url": "/code-class/static/js/2.4ce6849d.chunk.js"
+  },
+  {
+    "revision": "bc1a73d08d8433691746793d8481dea3",
+    "url": "/code-class/static/js/2.4ce6849d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddb77bfc314115d4bed",
+    "url": "/code-class/static/js/main.1fc74b0d.chunk.js"
+  },
+  {
+    "revision": "2f3854a130e70e4db96e",
+    "url": "/code-class/static/js/runtime-main.cad02683.js"
+  }
+]);
