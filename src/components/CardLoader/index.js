fb53eebf35391730789fbe7bd9b2ca3bd6d79cab
Dat Nguyen <51848355+tandat198@users.noreply.github.com>
2020-07-18 23:58:18 +0700

    Update index.js

--- src/components/CardLoader/index.js
+++ src/components/CardLoader/index.js
@@ -6,11 +6,11 @@ const CardLoader = (props) => {
     for (let i = 0; i < props.numberOfCards; i++) {
         listCard.push(
             <ContentLoader key={i} className='card-item' viewBox='0 0 300 41'>
-                <rect x='0' y='0' rx='5' ry='5' width='40' height='40' />
-                <rect x='45' y='0' rx='4' ry='4' width='180' height='10' />
-                <rect x='45' y='15' rx='3' ry='3' width='150' height='8' />
-                <rect x='45' y='28' rx='3' ry='3' width='50' height='5' />
-                <rect x='45' y='36' rx='3' ry='3' width='50' height='5' />
+                <rect x='0' y='0' rx='5' ry='5' width='45' height='45' />
+                <rect x='50' y='0' rx='4' ry='4' width='180' height='10' />
+                <rect x='50' y='15' rx='3' ry='3' width='150' height='8' />
+                <rect x='50' y='28' rx='3' ry='3' width='50' height='5' />
+                <rect x='50' y='36' rx='3' ry='3' width='50' height='5' />
             </ContentLoader>
         );
     }
