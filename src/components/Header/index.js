dd9c1e7ba128ca5ccf1ed2d7a691d57f182a7433
tandat198 <tandat198@gmail.com>
2020-08-17 14:42:30 +0700

    change navbar in small screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import "./style.css";
-import { Button, Collapse, Container, Nav, Navbar, NavbarToggler } from "shards-react";
-import { NavLink } from "react-router-dom";
+import { Button, Collapse, Nav, Navbar, NavbarToggler } from "shards-react";
+import { Link, NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { signOut } from "../../redux/user/actions";
 
 const Header = () => {
     const [collapseOpen, setCollapseOpen] = useState(false);
+    const [openAccountMenu, setOpenAccountMenu] = useState(false);
     const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
     const dispatch = useDispatch();
     const signOutReq = () => {
-        toggleNavbar();
+        setOpenAccountMenu(false);
+        setCollapseOpen(false);
         dispatch(signOut());
     };
     const toggleNavbar = () => {
@@ -20,61 +22,83 @@ const Header = () => {
             setCollapseOpen(!collapseOpen);
         }
     };
+    const toggleAccountMenu = () => {
+        setOpenAccountMenu(!openAccountMenu);
+    };
 
     return (
         <Navbar type='light' theme='white' expand='md'>
-            <Container fluid className='bg-white'>
+            <div className='d-flex'>
+                <NavbarToggler onClick={toggleNavbar} />
                 <NavLink onClick={() => setCollapseOpen(false)} to='/' className='brand text-dark text-decoration-none'>
                     Code Class
                 </NavLink>
-                <NavbarToggler onClick={toggleNavbar} />
-                <Collapse className='justify-content-end bg-white' open={collapseOpen} navbar>
-                    <Nav navbar>
+            </div>
+
+            <Collapse className='justify-content-end bg-white' open={collapseOpen} navbar>
+                <Nav navbar>
+                    <li className='nav-item d-flex align-items-center justify-content-center'>
+                        <NavLink onClick={toggleNavbar} className='nav-link' to='/mentors'>
+                            Mentor
+                        </NavLink>
+                    </li>
+                    {isAuthenticated ? (
                         <li className='nav-item d-flex align-items-center justify-content-center'>
-                            <NavLink onClick={toggleNavbar} className='nav-link' to='/'>
-                                Tutorial
+                            <NavLink onClick={toggleNavbar} className='nav-link' to='/users/saved-tutorials'>
+                                Saved Tutorial
                             </NavLink>
                         </li>
-                        <li className='nav-item d-flex align-items-center justify-content-center'>
-                            <NavLink onClick={toggleNavbar} className='nav-link' to='/mentors'>
-                                Mentor
+                    ) : null}
+                    {isAuthenticated ? null : (
+                        <li className='nav-item d-flex justify-content-center'>
+                            <NavLink onClick={toggleNavbar} className='nav-link' to='/sign-up'>
+                                <Button pill>Đăng ký</Button>
                             </NavLink>
                         </li>
-                        {isAuthenticated ? (
-                            <li className='nav-item d-flex align-items-center justify-content-center'>
-                                <NavLink onClick={toggleNavbar} className='nav-link' to='/users/saved-tutorials'>
-                                    Saved Tutorial
-                                </NavLink>
-                            </li>
-                        ) : null}
-                        {isAuthenticated ? null : (
-                            <li className='nav-item d-flex justify-content-center'>
-                                <NavLink onClick={toggleNavbar} className='nav-link' to='/sign-up'>
-                                    <Button pill>Đăng ký</Button>
-                                </NavLink>
-                            </li>
-                        )}
-                        {isAuthenticated ? null : (
-                            <li className='nav-item d-flex justify-content-center'>
-                                <NavLink onClick={toggleNavbar} className='nav-link' to='/sign-in'>
-                                    <Button pill outline>
-                                        Đăng nhập
-                                    </Button>
-                                </NavLink>
-                            </li>
-                        )}
-                        {isAuthenticated ? (
-                            <li className='nav-item d-flex justify-content-center'>
-                                <NavLink onClick={signOutReq} className='nav-link' to='/'>
-                                    <Button pill outline>
-                                        Đăng xuất
-                                    </Button>
-                                </NavLink>
-                            </li>
-                        ) : null}
-                    </Nav>
-                </Collapse>
-            </Container>
+                    )}
+                    {isAuthenticated ? null : (
+                        <li className='nav-item d-flex justify-content-center'>
+                            <NavLink onClick={toggleNavbar} className='nav-link' to='/sign-in'>
+                                <Button pill outline>
+                                    Đăng nhập
+                                </Button>
+                            </NavLink>
+                        </li>
+                    )}
+                    {isAuthenticated ? (
+                        <li className='nav-item d-flex justify-content-center'>
+                            <NavLink onClick={signOutReq} className='nav-link' to='/'>
+                                <Button pill outline>
+                                    Đăng xuất
+                                </Button>
+                            </NavLink>
+                        </li>
+                    ) : null}
+                </Nav>
+            </Collapse>
+            {isAuthenticated && <span role='button' onClick={toggleAccountMenu} className='profile-image'></span>}
+            {openAccountMenu && isAuthenticated ? (
+                <div>
+                    <div className='arrow-up'></div>
+                    <ul className='account-menu bg-primary'>
+                        <li>
+                            <Link
+                                onClick={toggleAccountMenu}
+                                className='text-white text-decoration-none'
+                                to='/users/saved-tutorials'
+                            >
+                                Bài đã lưu
+                            </Link>
+                        </li>
+                        <li className='text-white'>Cập nhật thông tin</li>
+                        <li>
+                            <Link onClick={signOutReq} className='text-white text-decoration-none' to='/'>
+                                Đăng xuất
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            ) : null}
         </Navbar>
     );
 };
--- src/pages/ChooseMentorPage/index.js
+++ src/pages/ChooseMentorPage/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./style.scss";
+import ContentLoader from "react-content-loader";
 import { useDispatch, useSelector } from "react-redux";
 import { getMentorsList } from "../../redux/mentor/actions";
 import { Badge, Button, Container, Modal, ModalBody, ModalFooter, ModalHeader } from "shards-react";
@@ -7,8 +8,10 @@ import { Badge, Button, Container, Modal, ModalBody, ModalFooter, ModalHeader } 
 export default function ChooseMentorPage() {
     const dispatch = useDispatch();
     const mentorsList = useSelector((state) => state.mentor.mentorsList);
+    const isLoading = useSelector((state) => state.mentor.isLoading);
     const [modalInfoIsOpening, setModalInfoIsOpening] = useState(false);
     const [activeMentor, setActiveMentor] = useState({});
+    const isLargeScreen = window.innerWidth > 576;
 
     const openModalInfo = (mentor) => {
         setModalInfoIsOpening(true);
@@ -22,38 +25,113 @@ export default function ChooseMentorPage() {
         dispatch(getMentorsList());
     }, [dispatch]);
 
+    const LoaderList = () => {
+        const list = [];
+        const rOfCicle = isLargeScreen ? "15" : "38";
+        const xOfFirstTextLine = isLargeScreen ? "50" : "135";
+        const yOfFirstTextLine = isLargeScreen ? "0" : "85";
+        const heightOfFirstTextLine = isLargeScreen ? "8" : "14";
+        const widthOfFirstTextLine = isLargeScreen ? "100" : "110";
+        const xOfSecondTextLine = isLargeScreen ? "50" : "100";
+        const yOfSecondTextLine = isLargeScreen ? "15" : +yOfFirstTextLine + +heightOfFirstTextLine + 10;
+        const heightOfSecondTextLine = isLargeScreen ? "8" : "14";
+        const widthOfSecondTextLine = isLargeScreen ? "150" : "180";
+        const xOfFirstBtn = isLargeScreen ? "322" : "0";
+        const yOfFirstBtn = isLargeScreen ? "0" : +yOfSecondTextLine + +heightOfSecondTextLine + 20;
+        const widthOfFirstBtn = isLargeScreen ? "58" : "180";
+        const heighOfFirstBtn = isLargeScreen ? "13" : "30";
+        const xOfSecondBtn = isLargeScreen ? "322" : 380 - widthOfFirstBtn;
+        const yOfSecondBtn = isLargeScreen ? "20" : yOfFirstBtn;
+
+        for (let i = 0; i < 8; i++) {
+            list.push(
+                <ContentLoader
+                    key={i}
+                    className='list-item'
+                    viewBox={isLargeScreen ? "0 0 380 33" : "0 0 380 200"}
+                    backgroundColor='#bdbdbd'
+                    foregroundColor='#ccc'
+                >
+                    <circle cx={isLargeScreen ? rOfCicle : "190"} cy={rOfCicle} r={rOfCicle} />
+                    <rect
+                        x={xOfFirstTextLine}
+                        y={yOfFirstTextLine}
+                        rx='4'
+                        ry='4'
+                        width={widthOfFirstTextLine}
+                        height={heightOfFirstTextLine}
+                    />
+                    <rect
+                        x={xOfSecondTextLine}
+                        y={yOfSecondTextLine}
+                        rx='3'
+                        ry='3'
+                        width={widthOfSecondTextLine}
+                        height={heightOfSecondTextLine}
+                    />
+                    <rect
+                        x={xOfFirstBtn}
+                        y={yOfFirstBtn}
+                        rx='3'
+                        ry='3'
+                        width={widthOfFirstBtn}
+                        height={heighOfFirstBtn}
+                    />
+                    <rect
+                        x={xOfSecondBtn}
+                        y={yOfSecondBtn}
+                        rx='3'
+                        ry='3'
+                        width={widthOfFirstBtn}
+                        height={heighOfFirstBtn}
+                    />
+                </ContentLoader>
+            );
+        }
+        return list;
+    };
+
     return (
         <div className='choose-mentor-page'>
             <Container>
-                <div className='mentors-list'>
-                    {mentorsList.map((mentor) => (
-                        <div className='list-item' key={mentor.id}>
-                            <div className='mentor-image'></div>
-                            <div className='mentor-info'>
-                                <div className='mb-1'>{mentor.user.name}</div>
-                                {mentor.user.dateOfBirth ? (
+                {isLoading ? (
+                    <LoaderList />
+                ) : (
+                    <div className='mentors-list'>
+                        {mentorsList.map((mentor) => (
+                            <div className='list-item' key={mentor.id}>
+                                <div className='mentor-image'>
+                                    <img
+                                        src='https://avatars2.githubusercontent.com/u/51848355?s=460&u=bf232c01c5a0c42352ef40d250ee13af51284a66&v=4'
+                                        alt=''
+                                    />
+                                </div>
+                                <div className='mentor-info'>
+                                    <div className='mb-1'>{mentor.user.name}</div>
+                                    {mentor.user.dateOfBirth ? (
+                                        <div className='mb-1'>
+                                            Năm sinh: {new Date(mentor.user.dateOfBirth).getFullYear()}
+                                        </div>
+                                    ) : null}
                                     <div className='mb-1'>
-                                        Năm sinh: {new Date(mentor.user.dateOfBirth).getFullYear()}
+                                        <span className='mr-2'>Có thể mentor</span>
+                                        {mentor.specialities.map((speciality) => (
+                                            <Badge key={speciality} className='mr-1' theme='dark'>
+                                                {speciality}
+                                            </Badge>
+                                        ))}
                                     </div>
-                                ) : null}
-                                <div className='mb-1'>
-                                    <span className='mr-2'>Có thể mentor</span>
-                                    {mentor.specialities.map((speciality) => (
-                                        <Badge key={speciality} className='mr-1' theme='dark'>
-                                            {speciality}
-                                        </Badge>
-                                    ))}
+                                </div>
+                                <div className='buttons-wp d-flex'>
+                                    <Button className='mb-1'>Chọn Mentor</Button>
+                                    <Button className='mt-1' theme='light' onClick={() => openModalInfo(mentor)}>
+                                        Thêm Thông Tin
+                                    </Button>
                                 </div>
                             </div>
-                            <div className='buttons-wp d-flex'>
-                                <Button className='mb-1'>Chọn Mentor</Button>
-                                <Button className='mt-1' theme='light' onClick={() => openModalInfo(mentor)}>
-                                    Thêm Thông Tin
-                                </Button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
             </Container>
             <Modal open={modalInfoIsOpening} toggle={closeModalInfo} backdropClassName='backdrop-info-modal'>
                 <ModalHeader>Thông tin Mentor</ModalHeader>
